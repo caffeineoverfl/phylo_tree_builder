63a186543aa64f2afbc63e23d789b46081d420f0
caffeineoverfl <tonikrug2000@icloud.com>
2025-05-17 17:23:43 +0200

    - Added parser module in main.py - fixed a bug with the output file in aligner.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from src import parser, aligner, distance_matrix, tree_builder, visualizer
 
 def main():
     input_fasta = "data/example_dna.fasta"
+
+    # use the parser module
+    sequences = parser.read_fasta(input_fasta)
+    print(f"Loaded {len(sequences)} sequences:")
+    for seq_id, sequence in sequences.items():
+        print(f"  {seq_id}: {len(sequence)} bp")
+
+    # Run alignment
     aligned = aligner.run_clustal_omega(input_fasta)
     dist_matrix = distance_matrix.compute_distance_matrix(aligned)
     tree = tree_builder.build_tree(dist_matrix)
